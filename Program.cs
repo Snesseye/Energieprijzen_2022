92127e2645e3ddeafaec2a945554768aa927379d
Snesseye <joaquingoossens@hotmail.com>
2022-09-21 09:43:18 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Energieprijzen_2022
+{
+    class Program
+    {
+        static void Main(string[] args) // © Joaquin Goossens
+        {
+            double vermogen, tijd, prijs, jaar, totaalprijs;
+
+            // Intro
+            Console.WriteLine("Berkent gemiddelde kostprijs computer op jaarbasis");
+            Console.WriteLine();
+            // Invoer
+            Console.Write("Gemiddeld vermogen (in watt) : ");
+            vermogen = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Gemiddelde computertijd per dag (in uur) : ");
+            tijd = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Prijs energieleverancier (in euro per kWh) : ");
+            prijs = Convert.ToDouble(Console.ReadLine());
+
+            // Formule
+            jaar = tijd * 365;
+            totaalprijs = vermogen / 1000 * jaar * prijs;
+
+            Console.WriteLine();    
+            Console.WriteLine("De gemiddelde kostprijs voor je computer op jaarbasis is: {0} euro", totaalprijs);
+            Console.ReadLine();
+        }
+    }
+}
